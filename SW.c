0b02c0b1f2bcd00fae25920258d105f9ebb37c98
Ahmed Elbanawi <elbanawiahmed@gmail.com>
2020-11-12 23:00:28 +0200

    Create SW.c

---
+++ SW.c
@@ -0,0 +1,30 @@
+/******************************************************************************/
+#ifndef SW_H
+#include "SW.h"
+#endif
+/******************************************************************************/
+void Init(SW_t* const SW){
+     GPIO_SetPinDirection(SW->Port, SW->Pin, GPIO_INPUT);
+}
+/******************************************************************************/
+void Read(SW_t* const SW){
+     switch(SW->Time){
+            case SW_FALLING:
+                 SW->State = SW_STATE_OFF;
+                 if(GPIO_LOW == GPIO_ReadPinValue(SW->Port, SW->Pin)){
+                    SW->Time = SW_RISING;
+                 }
+                 break;
+            case SW_RISING:
+                 if(GPIO_HIGH == GPIO_ReadPinValue(SW->Port, SW->Pin)){
+                    SW->State = SW_STATE_ON;
+                    SW->Time = SW_FALLING;
+                 }
+                 break;
+            default:
+                 /* Error: Undefined SW Time */
+                 break;
+                 
+     }
+}
+/******************************************************************************/
