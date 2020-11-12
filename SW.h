7498ca5f1818e671a708a3087451da80438698ab
Ahmed Elbanawi <elbanawiahmed@gmail.com>
2020-11-12 22:59:21 +0200

    Create SW.h

---
+++ SW.h
@@ -0,0 +1,27 @@
+/******************************************************************************/
+#ifndef SW_H
+#define SW_H
+#endif
+/******************************************************************************/
+#ifndef GPIO_H
+#include "GPIO.h"
+#endif
+/******************************************************************************/
+typedef enum{
+        SW_STATE_OFF = 0,
+        SW_STATE_ON
+}SW_STATE_t;
+typedef enum{
+        SW_FALLING = 0,
+        SW_RISING
+}SW_TIME_t;
+typedef struct{
+        GPIO_Port_ID_t Port  :  3;
+        GPIO_Pin_ID_t  Pin   :  3;
+        SW_TIME_t      Time  :  1;
+        SW_STATE_t     State :  1;
+}SW_t;
+/******************************************************************************/
+void Init(SW_t* const SW);
+void Read(SW_t* const SW);
+/******************************************************************************/
